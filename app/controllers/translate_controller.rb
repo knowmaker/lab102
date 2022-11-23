928a276de2fadc857c430838718e2f1db883f6cc
alexander <avoronin2004@gmail.com>
2022-11-23 21:48:00 +0300

    Initial

---
+++ app/controllers/translate_controller.rb
@@ -0,0 +1,13 @@
+class TranslateController < ApplicationController
+  def output
+    @chislo = params[:ch].to_i
+    @st = @chislo.to_s(2)
+    @rev_st = @st.reverse
+    @result = @rev_st.to_i(2)
+
+    respond_to do |format|
+      format.xml {render xml: {chislo: @chislo, st: @st, rev_st: @rev_st, result: @result}.to_xml}
+      format.rss {render xml: {chislo: @chislo, st: @st, rev_st: @rev_st, result: @result}.to_xml}
+    end
+  end
+end
